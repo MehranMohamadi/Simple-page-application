6e423e4eb8e67de4dab5d212b84611acaeb9d9e1
Mehran Mohammadi <mohammadi000098@gmail.com>
2021-05-09 20:05:30 +0430

    change router

--- src/router.js
+++ src/router.js
@@ -1,7 +1,9 @@
 import {createRouter,createWebHistory} from 'vue-router'
+import Home from "./components/sections/Home";
 
-
-const routes=[];
+const routes=[
+    {path: "/",name:"Home",components:Home},
+];
 
 const router=createRouter({
     history:createWebHistory(),
