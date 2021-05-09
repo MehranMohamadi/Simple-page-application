43c3ac2f456fc178bd1679e5ab2a88b2e75e21fd
Mehran Mohammadi <mohammadi000098@gmail.com>
2021-05-09 19:57:25 +0430

    navbar

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -9,7 +9,18 @@
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <router-link class="nav-link"  to="/">Home</router-link>
+                        </li>
+                        <li class="nav-item">
+                           <router-link class="nav-link"  to="/users">users</router-link>
+                        </li>
+                        <li class="nav-item">
+                            <router-link class="nav-link" to="/posts">Posts</router-link>
+                        </li>
 
+                    </ul>
 
                 </div>
             </div>
