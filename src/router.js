f1640d335d0a72a76d440840b20c1edb545947da
Mehran Mohammadi <mohammadi000098@gmail.com>
2021-05-15 12:30:42 +0430

    add edit post component

--- src/router.js
+++ src/router.js
@@ -8,6 +8,7 @@ import Post from './components/sections/POSTS/POST';
 import showPost from "./components/sections/POSTS/showPost";
 import TemplatePost from './components/sections/POSTS/TemplatePost'
 import CreatePost from './components/sections/POSTS/createPost'
+import editPost from './components/sections/POSTS/editpost'
 
 const routes = [
     {path: "/", name: "home", component: Home},
@@ -22,7 +23,8 @@ const routes = [
         path: "/posts", name: "postTemplate", component: TemplatePost, children: [
             {path: '', name: "posts", component: Post},
             {path: ':id', name: "post_id", component: showPost},
-            {path: "CREATE", name: "createpost", component: CreatePost}
+            {path: "CREATE", name: "createpost", component: CreatePost},
+            {path: "edit/:id", name: "editpost", component: editPost}
         ]
     }
 ];
