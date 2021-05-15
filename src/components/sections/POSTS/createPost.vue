2d527e067a0dbe4c452c4f67d26a8526d4015577
Mehran Mohammadi <mohammadi000098@gmail.com>
2021-05-15 11:58:17 +0430

    send post method

--- src/components/sections/POSTS/createPost.vue
+++ src/components/sections/POSTS/createPost.vue
@@ -1,36 +1,41 @@
 <template>
     <div class="col-md-6 align-items-center">
         <form @submit.prevent="validate">
-        <div class="mb-3">
-            <label for="exampleInputEmail1" class="form-label">Title</label>
-            <input v-model.lazy="form.title" type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-            <div class="form-text text-danger">
-                {{form.titleerror}}
+            <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">Title</label>
+                <input v-model.lazy="form.title" type="text" class="form-control" id="exampleInputEmail1"
+                       aria-describedby="emailHelp">
+                <div class="form-text text-danger">
+                    {{form.titleerror}}
+                </div>
             </div>
-        </div>
 
-        <div class="mb-3">
-            <label for="exampleInputPassword1" class="form-label">Body</label>
-            <input v-model.lazy.trim="form.body" type="text" class="form-control" id="exampleInputPassword1" :placeholder="form.titleerror">
-            <div class="form-text text-danger">
-                {{form.bodyerror}}
+            <div class="mb-3">
+                <label for="exampleInputPassword1" class="form-label">Body</label>
+                <input v-model.lazy.trim="form.body" type="text" class="form-control" id="exampleInputPassword1"
+                       :placeholder="form.titleerror">
+                <div class="form-text text-danger">
+                    {{form.bodyerror}}
+                </div>
             </div>
-        </div>
 
 
-        <button type="submit" class="btn btn-primary">Post</button>
-    </form></div>
+            <button type="submit" class="btn btn-primary">Post</button>
+        </form>
+    </div>
 </template>
 
 <script>
-    import {reactive} from 'vue'
+    import {reactive} from 'vue';
+    import axios from 'axios';
+
     export default {
-        setup(){
-            const form=reactive({
-                title:"",
-                body:"",
-                titleerror:"",
-                bodyerror:""
+        setup() {
+            const form = reactive({
+                title: "",
+                body: "",
+                titleerror: "",
+                bodyerror: ""
             });
 
             function validate() {
@@ -44,9 +49,28 @@
                 } else {
                     form.bodyerror = ''
                 }
+                if (form.title !== "" && form.body !== "") {
+                    createpost()
+                }
             }
 
-            return{form,validate}
+            function createpost() {
+                axios.post('https://jsonplaceholder.typicode.com/posts', {
+                    title: form.title,
+                    body: form.body,
+                    userId: 1,
+                })
+                    .then(function (response) {
+                        // handle success
+                      console.log(response.data)
+                    })
+                    .catch(function (error) {
+                        // handle error
+                        console.log(error);
+                    })
+            }
+
+            return {form, validate}
         }
     }
 </script>
